0dfb615ee6f8dc3a3fd665fc87e3d876e3cd8224
Bernardo Anderson <bdsa@hey.com>
2024-04-10 18:20:46 -0500

    Add new icons to switch statement

--- bot.rb
+++ bot.rb
@@ -34,12 +34,11 @@ Telegram::Bot::Client.run(token) do |bot|
         when '/start', 'start', 'Start'
           bot.api.send_message(chat_id: message.chat.id, text: "Right on, #{message.from.first_name}", reply_markup: keyboard_markup)
         # MEDITATION
-        when '/meditation', 'meditation', 'Meditation'
-
+        when '/meditation', 'meditation', 'Meditation', '🧘 Meditation'
           # Ask how long they want to meditate
           bot.api.send_message(chat_id: message.chat.id, text: "https://www.youtube.com/watch?v=vHheEy-0a7o", reply_markup: keyboard_markup)
         # LEBOWSKI QUOTES
-        when '/lebowski', 'lebowski quote', 'Lebowski Quote'
+        when '/lebowski', 'lebowski quote', 'Lebowski Quote', '💬 Lebowski Quote'
           # Give a random quote for now
           url = 'https://www.thebiglebow.ski/api/v1/random/favorite'
           response = HTTParty.get(url)
@@ -47,7 +46,7 @@ Telegram::Bot::Client.run(token) do |bot|
           bot.api.send_photo(chat_id: message.chat.id, photo: response.parsed_response['still_with_text'])
           bot.api.send_message(chat_id: message.chat.id, text: response.parsed_response['quote'], reply_markup: keyboard_markup)
         # CAUCASIAN RECIPE
-        when '/recipe', 'Caucasian recipe', 'Caucasian Recipe', 'White Russian recipe', 'White Russian Recipe'
+        when '/recipe', 'Caucasian recipe', 'Caucasian Recipe', 'White Russian recipe', 'White Russian Recipe', '🍸 White Russian Recipe'
           # Send back the recipe text and an image
           photo_path = "media/lebowski_drink.jpeg"
           bot.api.send_photo(chat_id: message.from.id, photo: Faraday::UploadIO.new(photo_path, 'image/jpeg'))
@@ -56,12 +55,12 @@ Telegram::Bot::Client.run(token) do |bot|
           bot.api.send_message(chat_id: message.chat.id, text:
             "Fill a rocks glass with ice\nPour in two shots of vodka and three shots of Kahlúa\nTop off with milk\nStir and serve", reply_markup: keyboard_markup)
         # TAO TE CHING
-        when '/tao', 'tao', 'Read the Tao'
+        when '/tao', 'tao', 'Read the Tao', '📕 Read the Tao'
           markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: tao_verses)
           # Ask if they want a random verse or pick a specific one
           bot.api.send_message(chat_id: message.chat.id, text: "Pick a verse, #{message.from.first_name}", reply_markup: markup)
         # THE DUDE TESTAMENT
-        when '/testament', 'the dude testament', 'The Dude Testament'
+        when '/testament', 'the dude testament', 'The Dude Testament', '✍️ The Dude Testament'
           kb = [[
             Telegram::Bot::Types::InlineKeyboardButton.new(text: 'PDF', callback_data: 'pdf'),
             Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Audio', callback_data: 'audio')
@@ -72,7 +71,7 @@ Telegram::Bot::Client.run(token) do |bot|
         when '/stop', 'stop', 'Stop'
           kb = Telegram::Bot::Types::ReplyKeyboardRemove.new(remove_keyboard: true)
           bot.api.send_message(chat_id: message.chat.id, text: "Did I urinate on your rug, #{message.from.first_name}?", reply_markup: kb)
-        when 'books', 'Books', 'buy books', 'Buy books'
+        when 'books', 'Books', 'buy books', 'Buy books', '📚 Buy books'
           kb = [
             [
               Telegram::Bot::Types::InlineKeyboardButton.new(text: 'The Incomplete Dudeist Priest\'s Handbook', url: 'https://amzn.to/4azqII4')
@@ -91,7 +90,7 @@ Telegram::Bot::Client.run(token) do |bot|
           markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
           bot.api.send_message(chat_id: message.chat.id, text: "That's fucking interesting, man. That's fucking interesting.", reply_markup: markup)
         # HELP
-        when '/help', 'help', 'Help'
+        when '/help', 'help', 'Help', 'ℹ️ Help'
           # Send a list of commands
           bot.api.send_message(chat_id: message.chat.id, text: "Is this a-what day is it?\nThis bot is for helping Dudes follow the Dudeist path.", reply_markup: keyboard_markup)
         end
